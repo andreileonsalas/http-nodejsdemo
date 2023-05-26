4b8e2aa01ac7a789897d49f33afdd719043b05f1
andreileonsalas <andreileonsalas@gmail.com>
2023-05-25 20:53:23 -0600

    Update server.mjs

--- server.mjs
+++ server.mjs
@@ -1,6 +1,6 @@
 import { createServer } from 'http';
 
 createServer((req, res) => {
-  res.write('Hello World!');
+  res.write('Hello World! Hola demo');
   res.end();
 }).listen(process.env.PORT);
